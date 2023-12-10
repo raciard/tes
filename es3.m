1c053ac8f7ed3e9f21067cb4f8c11d099d0962bf
Riccardo Bracciale <raciard@raciard.com>
2023-12-10 15:50:19 +0100

    cambiato es23

--- es3.m
+++ es3.m
@@ -1,30 +1,46 @@
-n = -5000000:5000000;
+n = -500:500; % per il plot n = -10:10
 n_0 = 2;
 A = 5;
 N = 6;
 
 % Inizializzazione funzioni
 xa = exp(-n) .* (n >= 0);
-xb = A;
+xb = A .* ones(1,length(n));
 xc = A * exp(-(1i * 2 * pi * n) / N);
 xd = A .* (n >= 0);
-xe = A .* ((n > (n_0 - N)) & (n <= (n_0 + N)));
+xe = A .* ((n >= (n_0 - N)) & (n <= (n_0 + N)));
 
 % Calcolo energia e potenza per ciascun segnale
 E_xa = sum(abs(xa).^2);
 P_xa = E_xa / length(n);
+subplot(3,2,1)
+stem(n, xa)
+title("e^-n")
+
 
 E_xb = sum(abs(xb).^2);
 P_xb = E_xb / length(n);
+subplot(3,2,2)
+stem(n, xb)
+title("A")
 
 E_xc = sum(abs(xc).^2);
 P_xc = E_xc / length(n);
+subplot(3,2,3)
+stem(n, xa)
+title("Ae^(-j2πn/N)")
 
 E_xd = sum(abs(xd).^2);
 P_xd = E_xd / length(n);
+subplot(3,2,4)
+stem(n, xd)
+title("A per n >= 0")
 
 E_xe = sum(abs(xe).^2);
 P_xe = E_xe / length(n);
+subplot(3,2,5)
+stem(n, xe)
+title("A per [n-n0] <= N")
 
 % Visualizzazione dei risultati
 disp('Energia e potenza dei segnali:');
